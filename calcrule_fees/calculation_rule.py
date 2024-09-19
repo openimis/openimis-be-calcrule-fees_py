8507e6f70cf65a73a63aa6d46ad1b20243aff041
delcroip <patrick.delcroix@swisstph.ch>
2024-09-19 14:01:35 +0200

    fix uuid check

--- calcrule_fees/calculation_rule.py
+++ calcrule_fees/calculation_rule.py
@@ -45,9 +45,9 @@ class FeesCalculationRule(AbsStrategy):
         class_name = instance.__class__.__name__
         match = False
         if class_name == "ABCMeta":
-            match = UUID(cls.uuid) == UUID(instance.uuid)
+            match = UUID(str(cls.uuid)) == UUID(str(instance.uuid))
         if class_name == "PaymentPlan":
-            match = UUID(cls.uuid) == UUID(instance.calculation)
+            match = UUID(str(cls.uuid)) == UUID(str(instance.calculation))
         elif class_name == "BatchRun":
             # BatchRun → Product or Location if no prodcut
             match = cls.check_calculation(instance.location)
